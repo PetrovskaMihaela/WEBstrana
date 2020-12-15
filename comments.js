35a7be4cdca0de09bd895caa964e688977c815d5
PetrovskaMihaela <mihaela.petrovska2@gmail.com>
2020-12-15 14:52:36 +0100

    Final 3.0

--- comments.js
+++ comments.js
@@ -1,57 +1,23 @@
 
 // OBID ZA KOMENTARI NA SEKOJA SLIKA
 
+// reseno, raboti 
 
-/*var comments = document.getElementsByClassName("comment")
 var titles = [[],[],[],[],[],[],[],[],[]];
-var titleInput = document.getElementsByClassName("title");
-var messageBox = document.getElementsByClassName("display");
+var titleInputBoxes = document.getElementsByClassName("title");
+var displayBoxes = document.getElementsByClassName("display");
 
-function Allow(clickedId)
-{ 
-   var i;
-   var titlespom=[];
-   titlespom=titles[clickedId];
-   for (i = 0; i < comments.length; i++) {
-      if(clickedId == comments[i]){
-         var _id=i;
-         if (!getElementById("_id").value.match(/[a-zA-Z]$/) && getElementById("_id").value !="")
-            {
-               getElementById("_id").value="";
-               alert("Please Enter only alphabets");
-             }
-   
-   window.location.reload()
-}}}
-
-
-function insert (clickedId) {
-   var i;
-   var titlespom=[];
-   titlespom = titles[clickedId-11];
-   for (i = 0; i < comments.length; i++) {
-      if(clickedId == comments[i]){
-         var __id=i;
-         titlespom.push(document.getElementById("_id").value);
-         clearAndShow(__id);
-      }
-   }
+function insert (number) {
+   var i = number;
+   titles[i-1].push(titleInputBoxes[i-1].value);
+   clearAndShow(i-1);
 }
 
-
-function clearAndShow(n) {
-   var i;
-   var titlespom=[];
-   titlespom=titles[clickedId-50];
-   for (i = 0; i < comments.length; i++) {
-      if(n == comments[i]){
-         var _id=i;
-         document.getElementById("_id").value="";
-         document.getElementById("50 + _id").messageBox.innerHTML="";
-         document.getElementById("50 + _id").messageBox.innerHTML += " " + titlespom.join("<br/> ") + "<br/>";
-      }  
-   }
+function clearAndShow (number){
+   var i = number;
+   titleInputBoxes[i].value = "";
+   displayBoxes[i].innerHTML = "";
+   displayBoxes[i].innerHTML += " " + titles[i].join("<br/> ") + "<br/>";
+   titles[i].value = "";
 }
-
-
 
--- likes.js
+++ likes.js
@@ -1,23 +1,35 @@
 
-function clickCounter() {
+
+var results = document.getElementsByClassName("result");
+//var count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0, count7 = 0, count8 = 0, count9 = 0;
+
+window.localStorage.setItem("l1", JSON.stringify(count1));
+window.localStorage.setItem("l2", JSON.stringify(count2));
+window.localStorage.setItem("l3", JSON.stringify(count3));
+window.localStorage.setItem("l4", JSON.stringify(count4));
+window.localStorage.setItem("l5", JSON.stringify(count5));
+window.localStorage.setItem("l6", JSON.stringify(count6));
+window.localStorage.setItem("l7", JSON.stringify(count7));
+window.localStorage.setItem("l8", JSON.stringify(count8));
+window.localStorage.setItem("l9", JSON.stringify(count9));
+
+
+function clickCounter(num) {
+    n = num-1;
     if (typeof(Storage) !== "undefined") {
-      if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      if (true) {
+        newCount = Number( localStorage.getItem("l"+num)) +1;
+        localStorage.setItem("l" + JSON.stringify(num) , JSON.stringify(newCount));
       } else {
-        localStorage.clickcount = 1;
-      }
-      document.getElementById("result").innerHTML = "This photo has " + localStorage.clickcount + " likes!";
+        newCount = 1;
+        localStorage.setItem("l"+num, JSON.stringify(newCount));
+      } 
+      results[n].innerHTML = "This photo has  "+ localStorage.getItem("l" + JSON.stringify(num))+"  likes!";
    } else {
-      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+      results[n].innerHTML = "Sorry, your browser does not support web storage...";
     }
   }
 
 
 
-  var identifikacija;
-  var likes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  function countLikes(clicked_id) {
-      likes[clicked_id-1]++;
-      identifikacija = "r"+clicked_id;
-      document.getElementById(identifikacija).innerHTML = likes[clicked_id-1];
-  }
+ 
